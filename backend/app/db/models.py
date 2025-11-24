855a91b6a808e9a60f0101d46c3d002b0a2ab8e8
AbdessamadTzn <abdessamadtouzani07@gmail.com>
2025-11-24 23:06:54 +0100

    Feat: Models Logic

---
+++ backend/app/db/models.py
@@ -0,0 +1,34 @@
+from typing import Optional
+from enum import Enum
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+
+
+# Define Roles
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    WRITER = "writer"
+    USER = "user"
+
+# Main User Table
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    email: str = Field(unique=True, index=True)
+    hashed_password: str
+    full_name: str = None
+    role: UserRole = Field(default=UserRole.USER)
+    is_active: bool = Field(default=True)
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+
+# Schema for SIGN UP
+class UserCreate(SQLModel):
+    full_name: str
+    email: str 
+    password: str
+
+# Schema for Response
+class UserRead(SQLModel):
+    id: int
+    email: str
+    full_name: str
+    role: UserRole
